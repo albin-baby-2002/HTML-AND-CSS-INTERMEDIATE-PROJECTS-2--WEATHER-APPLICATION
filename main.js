c48ae2f66ca042d6313cae261b4d80ef4d0cc3cb
Albin_Baby <131413750+albin-baby-2002@users.noreply.github.com>
2023-05-22 15:09:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { getWeather } from "./weather";
-import { iconMap } from "./iconmap";
+import { getWeather } from "/weather.js";
+import { iconMap } from "/iconmap.js";
 
 
 getWeather(10, 10 , Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).catch(e=> {console.log(e),alert("error getting weather")})
